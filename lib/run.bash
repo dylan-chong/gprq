4a455daeb7aeaa5d8b637f641b2e912ae3201aea
Dylan Chong <dylan.chong@icloud.com>
2022-07-26 01:14:54 +1200

    Refactor/return early (#5)
    refactor: return early

--- lib/run.bash
+++ lib/run.bash
@@ -8,60 +8,64 @@ function main() {
     # TODO tidy
     read -p "Are you on the right base commit *and* does this show the right staged files [y/n]? " CONT
     echo
-    if [ "$CONT" = "y" ]; then
-        if [ -z "$1" ]; then
-            # Take commit message from clipboard so you can copy the jira ticket number and description straight after it
-            local message=`pbpaste | tr '\n' ' ' | perl -pe 's/\s+/ /g'`
-            local branch=`echo "$message" | commit_message_to_branch`
-        else
-            # Check if argument is branch name or commit message by if it has
-            # no spaces and a / or _ or - in it
-            if [[ "$@" =~ ^[A-Za-z0-9_-]+[/_-][A-Za-z0-9/_-]+$ ]]; then
-                # Argument was branch name
-                local branch="$1"
 
-                # If contains a slash
-                if [[ "$1" =~ / ]]; then
-                    local prefix=`echo $branch | perl -pe 's/\/.*//'`
-                    local prefix_formatted="$prefix"
-                    local separator=': '
-                    local suffix=${branch#"$prefix"/}
-                else
-                    local prefix_formatted=''
-                    local separator=''
-                    local suffix="$branch"
-                fi
-
-                # Pass branch suffix as argument and convert to commit messagee
-                # 1. Replace all - and _ with spaces
-                # 2. Replace 2+ spaces with ' - ' so you can use '--' in the branch name represent an actual dash
-                local suffix_formatted=`echo "$suffix" | perl -pe 's/_|-/ /g' | perl -pe 's/\s\s+/ - /g'`
-
-                local message="$prefix_formatted$separator$suffix_formatted"
-            else
-                # Argument was commit message
-                local message=`echo "$@" | perl -pe 's/^\s*//' | perl -pe 's/\s*$//'`
-                local branch=`echo "$message" | commit_message_to_branch`
-            fi
-        fi
-
-        echo "New Branch: $branch"
-        echo "Commit message: $message"
-        echo
-
-        read -p "Are these correct [y/n]? " CONT
-        echo
-        if [ "$CONT" = "y" ]; then
-            git checkout -b "$branch" \
-                && git commit -m "$message" \
-                && git push -u origin "$branch" \
-                && open_pull_request_in_browser
-        else
-            echo "Cancelling";
-        fi
-    else
+    if [ "$CONT" != "y" ]; then
         echo "Cancelling";
+        return
     fi
+
+    if [ -z "$1" ]; then
+        # Take commit message from clipboard so you can copy the jira ticket number and description straight after it
+        local message=`pbpaste | tr '\n' ' ' | perl -pe 's/\s+/ /g'`
+        local branch=`echo "$message" | commit_message_to_branch`
+    else
+        # Check if argument is branch name or commit message by if it has
+        # no spaces and a / or _ or - in it
+        if [[ "$@" =~ ^[A-Za-z0-9_-]+[/_-][A-Za-z0-9/_-]+$ ]]; then
+            # Argument was branch name
+            local branch="$1"
+
+            # If contains a slash
+            if [[ "$1" =~ / ]]; then
+                local prefix=`echo $branch | perl -pe 's/\/.*//'`
+                local prefix_formatted="$prefix"
+                local separator=': '
+                local suffix=${branch#"$prefix"/}
+            else
+                local prefix_formatted=''
+                local separator=''
+                local suffix="$branch"
+            fi
+
+            # Pass branch suffix as argument and convert to commit messagee
+            # 1. Replace all - and _ with spaces
+            # 2. Replace 2+ spaces with ' - ' so you can use '--' in the branch name represent an actual dash
+            local suffix_formatted=`echo "$suffix" | perl -pe 's/_|-/ /g' | perl -pe 's/\s\s+/ - /g'`
+
+            local message="$prefix_formatted$separator$suffix_formatted"
+        else
+            # Argument was commit message
+            local message=`echo "$@" | perl -pe 's/^\s*//' | perl -pe 's/\s*$//'`
+            local branch=`echo "$message" | commit_message_to_branch`
+        fi
+    fi
+
+    echo "New Branch: $branch"
+    echo "Commit message: $message"
+    echo
+
+    read -p "Are these correct [y/n]? " CONT
+    echo
+
+    if [ "$CONT" != "y" ]; then
+        echo "Cancelling";
+        return
+    fi
+
+    git checkout -b "$branch" \
+        && git commit -m "$message" \
+        && git push -u origin "$branch" \
+        && open_pull_request_in_browser
 }
 
 function open_pull_request_in_browser() {

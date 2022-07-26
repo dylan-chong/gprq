294e5946a884967a076ec5e22b1ed093828a9140
Dylan Chong <dylan.chong@icloud.com>
2022-07-26 15:55:21 +1200

    fix: directory not loaded correctly (#9)

--- define_gprq.bash
+++ define_gprq.bash
@@ -1,5 +1,6 @@
+# Copied from https://stackoverflow.com/a/246128/1726450
+GPRQ_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
 gprq() {
-    # Copied from https://stackoverflow.com/a/246128/1726450
-    script_dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-    "$script_dir/lib/run.bash" $@
+    "$GPRQ_DIR/lib/run.bash" $@
 }

b4586025c5503b9024774672d63f9a06fc8cfe99
Dylan Chong <dylan.chong@icloud.com>
2022-07-26 15:55:07 +1200

    fix: directory not loaded correctly

--- define_gprq.plugin.zsh
+++ define_gprq.plugin.zsh
@@ -1,1 +1,6 @@
-define_gprq.zsh
+# Copied from https://stackoverflow.com/a/246128/1726450
+GPRQ_DIR=$( cd -- "$( dirname -- "$0" )" &> /dev/null && pwd )
+
+gprq() {
+    "$GPRQ_DIR/lib/run.bash" $@
+}
--- define_gprq.zsh
+++
@@ -1,5 +0,0 @@
-gprq() {
-    # Copied from https://stackoverflow.com/a/246128/1726450
-    script_dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-    "$script_dir/lib/run.bash" $@
-}

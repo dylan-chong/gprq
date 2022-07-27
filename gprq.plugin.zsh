759fd75920c2066985da27fcfb7abba8aafa028e
Dylan Chong <dylan.chong@icloud.com>
2022-07-27 15:40:23 +1200

    fix: Oh my zsh plugin not found

--- gprq.plugin.zsh
+++ gprq.plugin.zsh
@@ -1,3 +1,6 @@
+# Defines the gprq function
+# This file is needed for oh-my-zsh
+
 # Copied from https://stackoverflow.com/a/246128/1726450
 GPRQ_DIR=$( cd -- "$( dirname -- "$0" )" &> /dev/null && pwd )
 

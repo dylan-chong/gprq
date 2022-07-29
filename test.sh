08a5df29b3ee540ccfbd5afe6783a2f18f64573b
Dylan Chong <dylan.chong@icloud.com>
2022-07-29 23:58:51 +1200

    refactor: Tests to use eval (#30)

--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 set -e
-source ./lib/functions.bash
+source ./lib/main.bash
 
 failed_tests=0
 
@@ -21,42 +21,43 @@ function expect_to_equal() {
 }
 
 function test() {
-    local fn="$1"
-    local fn_arg="$2"
-    local operator="$3"
-    local expected="$4"
+    local function_call_string="$1"
+    local operator="$2"
+    local expected="$3"
 
     if [ "$operator" != '==' ]; then
-        echo "Invalid operator '$operator' for test '$@'"
+        echo "Invalid operator '$operator' for test '$*'"
         return
     fi
 
-    local result=`$1 "$fn_arg"`
+    local result=`eval $function_call_string`
 
-    expect_to_equal "$result" "$expected" "$fn \"$fn_arg\""
+    expect_to_equal "$result" "$expected" "$function_call_string"
 }
 
-test commit_message_to_branch "hello world" == "hello-world"
-test commit_message_to_branch "hello world with lots of words" == "hello-world-with-lots-of-words"
-test commit_message_to_branch "hello: world stuff" == "hello/world-stuff"
-test commit_message_to_branch "Hello: World  stuff" == "hello/world-stuff"
-test commit_message_to_branch "Hello: World__stuff" == "hello/world-stuff"
-test commit_message_to_branch "Hello: world stuff" == "hello/world-stuff"
-test commit_message_to_branch "Hello: world stuff - part 1 - fix things" == "hello/world-stuff--part-1--fix-things"
-test commit_message_to_branch "Hello: world stuff with lots of words" == "hello/world-stuff-with-lots-of-words"
-test commit_message_to_branch "JIRA-123: Hello world stuff" == "JIRA-123/hello-world-stuff"
-test commit_message_to_branch "JIRA-123 Hello world stuff" == "JIRA-123/hello-world-stuff"
-test commit_message_to_branch "JIRA-123     Hello world stuff" == "JIRA-123/hello-world-stuff"
-test commit_message_to_branch "Testing stuff: Hello world" == "testing-stuff/hello-world"
+test 'commit_message_to_branch "hello world"' == "hello-world"
+test 'commit_message_to_branch "hello world with lots of words"' == "hello-world-with-lots-of-words"
+test 'commit_message_to_branch "hello: world stuff"' == "hello/world-stuff"
+test 'commit_message_to_branch "Hello: World  stuff"' == "hello/world-stuff"
+test 'commit_message_to_branch "Hello: World__stuff"' == "hello/world-stuff"
+test 'commit_message_to_branch "Hello: world stuff"' == "hello/world-stuff"
+test 'commit_message_to_branch "Hello: world stuff - part 1 - fix things"' == "hello/world-stuff--part-1--fix-things"
+test 'commit_message_to_branch "Hello: world stuff with lots of words"' == "hello/world-stuff-with-lots-of-words"
+test 'commit_message_to_branch "JIRA-123: Hello world stuff"' == "JIRA-123/hello-world-stuff"
+test 'commit_message_to_branch "JIRA-123 Hello world stuff"' == "JIRA-123/hello-world-stuff"
+test 'commit_message_to_branch "JIRA-123     Hello world stuff"' == "JIRA-123/hello-world-stuff"
+test 'commit_message_to_branch "Testing stuff: Hello world"' == "testing-stuff/hello-world"
 
+test 'branch_to_commit_message "hello-world"' == "Hello world"
+test 'branch_to_commit_message "hello-world-with-lots-of-words"' == "Hello world with lots of words"
+test 'branch_to_commit_message "hello/world-stuff"' == "hello: World stuff"
+test 'branch_to_commit_message "hello/world-stuff--part-1--fix-things"' == "hello: World stuff - part 1 - fix things"
+test 'branch_to_commit_message "hello/world-stuff-with-lots-of-words"' == "hello: World stuff with lots of words"
+test 'branch_to_commit_message "JIRA-123/hello-world-stuff"' == "JIRA-123: Hello world stuff"
+test 'branch_to_commit_message "testing-stuff/hello-world"' == "testing stuff: Hello world"
 
-test branch_to_commit_message "hello-world" == "Hello world"
-test branch_to_commit_message "hello-world-with-lots-of-words" == "Hello world with lots of words"
-test branch_to_commit_message "hello/world-stuff" == "hello: World stuff"
-test branch_to_commit_message "hello/world-stuff--part-1--fix-things" == "hello: World stuff - part 1 - fix things"
-test branch_to_commit_message "hello/world-stuff-with-lots-of-words" == "hello: World stuff with lots of words"
-test branch_to_commit_message "JIRA-123/hello-world-stuff" == "JIRA-123: Hello world stuff"
-test branch_to_commit_message "testing-stuff/hello-world" == "testing stuff: Hello world"
+# test 'reformat_clipboard_to_commit_message "hello world"' == "hello-world"
+# test 'reformat_clipboard_to_commit_message "sfdsf"' == "hello-world"
 
 echo
 

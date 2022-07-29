bd5a29b70a33223534fa9ed4a9db32cc6fbd54da
Dylan Chong <dylan.chong@icloud.com>
2022-07-29 23:18:06 +1200

    refactor: lib into multiple files

--- gprq.bash
+++ gprq.bash
@@ -1,14 +1,16 @@
+# To be `source`d so doesn't need a shebang line
+
 # Defines the gprq function
 # This file is needed for bash users
 
 # Copied from https://stackoverflow.com/a/246128/1726450
-GPRQ_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+export GPRQ_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 gprq() {
     cmd="\
         set -e \
-        && source $GPRQ_DIR/lib/functions.bash \
-        && main $@ \
+        && source $GPRQ_DIR/lib/main.bash \
+        && main \"$@\" \
         "
 
     bash -c "$cmd"
---
+++ lib/utils.bash
@@ -0,0 +1,33 @@
+function log() {
+    echo "$*" >> ~/Desktop/log
+}
+
+function trim_string() {
+    local string="$1"
+    echo "$string" | trim_string_pipe
+}
+
+function trim_string_pipe() {
+    perl -pe 's/^\s*//' | perl -pe 's/\s*$//'
+}
+
+function current_branch() {
+    git branch | awk '/^\* / { print $2 }'
+}
+
+# Format
+function f() {
+    if [ "$1" != "-b" ]; then
+        echo "Only -b" is supported
+    fi
+
+    printf "`bold`${@: 2}`not_bold`"
+}
+
+function bold() {
+    tput bold
+}
+
+function not_bold() {
+    tput sgr0
+}

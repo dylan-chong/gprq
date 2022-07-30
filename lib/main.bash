7ebe09eb4e436f18693ee6b3f2d5c330f4324414
Dylan Chong <dylan.chong@icloud.com>
2022-07-30 13:27:19 +1200

    test: for clipboard function (#32)
    * test: for clipboard function (WIP)
    
    * Delete unneeded tests, which failing due to using stdin not arg

--- lib/main.bash
+++ lib/main.bash
@@ -7,8 +7,7 @@ function main() {
     if [ -z "$1" ]; then
         # Take commit message from clipboard so you can copy the jira ticket number and description straight after it
         # MacOS specific
-        # TODO fn and tests
-        local message=`pbpaste | tr '\n' ' ' | perl -pe 's/\s+/ /g' | trim_string_pipe`
+        local message=`pbpaste | reformat_clipboard_to_commit_message`
         local branch=`commit_message_to_branch "$message"`
     else
         # Check if argument is branch name or commit message by if it has
